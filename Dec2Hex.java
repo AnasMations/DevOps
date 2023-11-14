cc869afc2709a86b617dbf54076b9140cb069fc7
Ubuntu <ubuntu@ip-172-31-87-205.ec2.internal>
2023-11-14 20:56:31 +0000

    SonarQube fixes

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,4 +1,46 @@
-import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class Dec2Hex {
+
+    private static final Logger log = Logger.getLogger(Dec2Hex.class.getName());
+
+    public static int Arg1;
+
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            log.severe("Error: no input provided!");
+            System.exit(1);
+            return;
+        }
+
+        try {
+            int decimalInput;
+            decimalInput = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            log.severe("Error: input provided is not an integer!");
+            System.exit(1);
+            return;
+        }
+
+        Arg1 = Integer.parseInt(args[0]);
+        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem;
+        int num;
+        num = Arg1;
+        String hexadecimal = "";
+        log.info("Converting the Decimal Value " + num + " to Hex...");
+
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
+        }
+
+        log.info("Converted Hexadecimal representation is: " + hexadecimal);
+    }
+}
+
+mport java.util.Scanner;
 
 class Dec2Hex
 {
@@ -7,20 +49,20 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-	if(args.length==0)
-	{
-		log.debug("Error: no input provided!");
-		System.exit(1);
-		return;
-	}
+        if(args.length==0)
+        {
+                log.debug("Error: no input provided!");
+                System.exit(1);
+                return;
+        }
 
-        try 
-	{
-	    int decimalInput;
+        try
+        {
+            int decimalInput;
             decimalInput = Integer.parseInt(args[0]);
-        } 
-	catch (NumberFormatException e) 
-	{
+        }
+        catch (NumberFormatException e)
+        {
             log.debug("Error: input provided is not integer!");
             System.exit(1);
             return;
@@ -29,7 +71,7 @@ class Dec2Hex
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem;
-	int num;
+        int num;
         num = Arg1;
         String hexadecimal="";
         log.debug("Converting the Decimal Value " + num + " to Hex...");
@@ -45,3 +87,4 @@ class Dec2Hex
 
     }
 }
+
