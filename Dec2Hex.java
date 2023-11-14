8b317227aacce326a8bd377d978254c596a2a438
Anas Ahmed <anas.ahassan@yahoo.com>
2023-11-14 21:56:57 +0200

    Initial Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,11 +7,6 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-	if(args.length==0)
-	{
-		System.err.println("Error: no input provided!");
-		System.exit(1);
-	}
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
