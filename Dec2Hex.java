e8c24e2b5ada574ed39c0182735b101128b0cdd2
Ubuntu <ubuntu@ip-172-31-87-205.ec2.internal>
2023-11-14 20:51:06 +0000

    SonarQube fixes

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -9,29 +9,30 @@ class Dec2Hex
     {
 	if(args.length==0)
 	{
-		System.out.println("Error: no input provided!");
+		log.debug("Error: no input provided!");
 		System.exit(1);
 		return;
 	}
 
-        int decimalInput;
         try 
 	{
+	    int decimalInput;
             decimalInput = Integer.parseInt(args[0]);
         } 
 	catch (NumberFormatException e) 
 	{
-            System.out.println("Error: input provided is not integer!");
+            log.debug("Error: input provided is not integer!");
             System.exit(1);
             return;
         }
 
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
+        int rem;
+	int num;
         num = Arg1;
         String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        log.debug("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
         {
@@ -40,7 +41,7 @@ class Dec2Hex
             num= num/16;
         }
 
-        System.out.println("Converted Hexadecimal representation is: " + hexadecimal);
+        log.debug("Converted Hexadecimal representation is: " + hexadecimal);
 
     }
 }
